02a6237a1ddcf5ea73f1ea2b8271dc58bfae3dd3
talhaHavadar <havadartalha@gmail.com>
2019-08-02 11:27:43 +0300

    Merge enum changes.

--- src/scenes/Home/index.js
+++ src/scenes/Home/index.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
-import './Home.css';
-import Line from '../../components/Line';
-import Toolbar from '../../components/Toolbar';
-import fs from '../../fs.json';
+import React, { Component } from "react";
+import "./Home.css";
+import Line from "../../components/Line";
+import Toolbar from "../../components/Toolbar";
+import fs from "../../fs.json"
+import { FSEntry } from "../../enums";
 
 class App extends Component {
   constructor() {
@@ -272,8 +273,8 @@ class App extends Component {
   trim = str => str.trimStart().trimEnd();
   removeSpaces = text => text.replace(/\s+/g, ' ').trim();
 
-  is_dir = obj => !!(obj && obj.type === 'directory');
-  is_file = obj => !!(obj && obj.type === 'file');
+  is_dir = obj => !!(obj && FSEntry.parse(obj.type) === FSEntry.DIRECTORY);
+  is_file = obj => !!(obj && FSEntry.parse(obj.type) === FSEntry.FILE);
 
   printCommandLine = () => this.cin(this.state.prompt_text);
   handleInputChange = e => this.setState({ prompt_text: e.target.value });
