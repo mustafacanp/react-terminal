f58ab0496e180e2645de288337746e511d699475
Mustafa Can Palaz <mustafacanpalaz@gmail.com>
2025-06-08 21:14:17 +0300

    Migrate to Vite from react-scripts; update package.json for new dependencies and scripts

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -21,18 +21,9 @@ const isLocalhost = Boolean(
 );
 
 export function register(config) {
-	if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-		// The URL constructor is available in all browsers that support SW.
-		const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
-		if (publicUrl.origin !== window.location.origin) {
-			// Our service worker won't work if PUBLIC_URL is on a different origin
-			// from what our page is served on. This might happen if a CDN is used to
-			// serve assets; see https://github.com/facebook/create-react-app/issues/2374
-			return;
-		}
-
+	if (import.meta.env.PROD && 'serviceWorker' in navigator) {
 		window.addEventListener('load', () => {
-			const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+			const swUrl = '/service-worker.js';
 
 			if (isLocalhost) {
 				// This is running on localhost. Let's check if a service worker still exists or not.
@@ -42,8 +33,7 @@ export function register(config) {
 				// service worker/PWA documentation.
 				navigator.serviceWorker.ready.then(() => {
 					console.log(
-						'This web app is being served cache-first by a service ' +
-							'worker. To learn more, visit http://bit.ly/CRA-PWA'
+						'This web app is being served cache-first by a service worker.'
 					);
 				});
 			} else {
@@ -71,7 +61,7 @@ function registerValidSW(swUrl, config) {
 							// content until all client tabs are closed.
 							console.log(
 								'New content is available and will be used when all ' +
-									'tabs for this page are closed. See http://bit.ly/CRA-PWA.'
+									'tabs for this page are closed.'
 							);
 
 							// Execute callback
---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [react()],
+	server: {
+		port: 3000,
+		open: true
+	},
+	build: {
+		outDir: 'build'
+	},
+	esbuild: {
+		loader: 'jsx',
+		include: /src\/.*\.[jt]sx?$/,
+		exclude: []
+	},
+	optimizeDeps: {
+		esbuildOptions: {
+			loader: {
+				'.js': 'jsx'
+			}
+		}
+	}
+});
