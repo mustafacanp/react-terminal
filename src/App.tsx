c18f35168b76e905c2fd73b794888aad8d3b46fa
Mustafa Can Palaz <mustafacanpalaz@gmail.com>
2025-06-20 18:10:52 +0300

    Prevent duplicate command entries in a row in history

--- src/App.tsx
+++ src/App.tsx
@@ -127,6 +127,9 @@ const App: React.FC = () => {
 
     const addToHistory = useCallback((command: string) => {
         setState(prev => {
+            if (prev.previousCommands[prev.previousCommands.length - 1] === command) {
+                return prev;
+            }
             const newHistory = [...prev.previousCommands, command];
             saveCommandHistoryToStorage(newHistory);
             return {
